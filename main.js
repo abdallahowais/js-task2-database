8cd765d3959e49b1b0f8ca46da874c592832aad5
abdallahowais <abdallah.owis@students.alquds.edu>
2024-01-22 19:54:37 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+async function getproducts() {
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+    const products = data.products
+
+    const result = products.map(function (ele) {
+        return `
+    <div class="product">
+        <h2>${ele.title}</h2>
+        <img src="${ele.thumbnail}"/>
+
+        <div class="prise-brand"> 
+        <h3>${ele.brand}<h3/>
+        <h3>${ele.price}<h3/>
+        </div>
+
+       
+    </div>
+
+    `;
+    }).join('');
+
+    document.querySelector(".products").innerHTML = result;
+};
+
+getproducts();
